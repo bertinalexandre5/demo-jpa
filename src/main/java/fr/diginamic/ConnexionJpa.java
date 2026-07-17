712112eca81e4b6083cabff9595a9555e7189db0
Alexandre Bertin <bertin.alexandre5@gmail.com>
2026-07-17 10:43:42 +0200

    TP 01 JPA -ok

---
+++ src/main/java/fr/diginamic/ConnexionJpa.java
@@ -0,0 +1,17 @@
+package fr.diginamic;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class ConnexionJpa {
+    public static void main(String[] args) {
+
+        EntityManagerFactory
+                entityManagerFactory = Persistence.createEntityManagerFactory("demo-jpa");
+        EntityManager
+                em = entityManagerFactory.createEntityManager();
+
+    }
+}
